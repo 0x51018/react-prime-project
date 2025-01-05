2792e2b9f4ff30f6038a74a3a75e1667ec7267f2
0x51018 <104045531+0x51018@users.noreply.github.com>
2025-01-05 23:42:50 +0900

    added pwa setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '소인수분해 계산기',
+        short_name: '소인수분해',
+        description: 'A Prime Factorization Calculator built with React',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: '',
-})
+});
