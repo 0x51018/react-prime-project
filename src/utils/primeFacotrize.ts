d588cd59f492650772b5abf0286d8b30ddd135ed
0x51018 <104045531+0x51018@users.noreply.github.com>
2025-01-06 00:32:39 +0900

    change title

--- src/utils/primeFacotrize.ts
+++ src/utils/primeFacotrize.ts
@@ -22,6 +22,31 @@ const primeFactorize = (n: number): Record<number, number> => {
   
     return result;
   };
+
+  // const primeFactorizeBigInt = (n: bigint): Record<string, number> => {
+  //   if (n <= 1n) {
+  //     return {};
+  //   }
+  
+  //   const factors: bigint[] = [];
+  //   let divisor = 2n;
+  
+  //   while (n > 1n) {
+  //     if (n % divisor === 0n) {
+  //       factors.push(divisor);
+  //       n /= divisor;
+  //     } else {
+  //       divisor++;
+  //     }
+  //   }
+  
+  //   const result = factors.reduce((acc, cur) => {
+  //     acc[cur.toString()] = (acc[cur.toString()] || 0) + 1;
+  //     return acc;
+  //   }, {} as Record<string, number>);
+  
+  //   return result;
+  // }
   
   export default primeFactorize;
   
